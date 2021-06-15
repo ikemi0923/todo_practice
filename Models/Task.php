7cd7b311115718eec254223a4e932139b7d4225b
ikemi hiroshi <hiroshi.ikemi@gmail.com>
2021-06-15 21:41:17 +0900

    commit

--- Models/Task.php
+++ Models/Task.php
@@ -22,7 +22,11 @@ class Task extends Model
         $stmt->execute($data);
     }
     // * update()を以下に追加する
-    
+    public function upddate($date)
+    {
+        $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . 'SET title = ?, contents = ? WHERE id = ?');
+        $stmt->execute($date);
+    }
 
 
 
--- store.php
+++ store.php
@@ -1,13 +1,15 @@
 <?php
 
 // ファイルの読み込み
-
+require_once('./Task.php');
 
 // データの受け取り
-
-
+$title = $_POST['title'];
+$contents = $_POST['contents'];
+$currentTime = date("Y/m/d H:i:s");
 // DBへのデータ保存
-
-
+$task = new Task();
+$task->create([$title, $contents, $currentTime]);
 // リダイレクト
-
+header('location:index.php');
+exit;
